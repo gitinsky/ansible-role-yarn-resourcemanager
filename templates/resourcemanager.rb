8e4f8931306a0762d4344fd7adf80b0877b58d50
Alex Chistyakov <alexclear@gmail.com>
2015-10-23 13:51:10 +0300

    Add tasks and templates

---
+++ templates/resourcemanager.rb
@@ -0,0 +1,17 @@
+Eye.application 'yarn-resourcemanager-{{ env_name }}' do
+  working_dir '/etc/eye'
+  stdall '/var/log/eye/yarn-resourcemanager-{{ env_name }}-stdall.log' # stdout,err logs for processes by default
+  trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
+  check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
+
+  process :resourcemanager_{{ env_name }} do
+    pid_file '{{ hadoop_var_prefix }}/yarn_resourcemanager.pid'
+    start_command 'sudo -u {{ hadoop_user }} {{ hadoop_distr_prefix }}/bin/yarn resourcemanager'
+
+    daemonize true
+    start_timeout 10.seconds
+    stop_timeout 5.seconds
+
+  end
+
+end
