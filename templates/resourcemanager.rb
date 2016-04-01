d25eff7c57965c784b78aba4d1147deb3b52fd85
Alex Chistyakov <alexclear@gmail.com>
2016-04-01 19:52:24 +0300

    Use uid setting

--- templates/resourcemanager.rb
+++ templates/resourcemanager.rb
@@ -3,10 +3,11 @@ Eye.application 'yarn-resourcemanager-{{ env_name }}' do
   stdall '/var/log/eye/yarn-resourcemanager-{{ env_name }}-stdall.log' # stdout,err logs for processes by default
   trigger :flapping, times: 10, within: 1.minute, retry_in: 3.minutes
   check :cpu, every: 10.seconds, below: 100, times: 3 # global check for all processes
+  uid "{{ hadoop_user }}"
 
   process :resourcemanager_{{ env_name }} do
     pid_file '{{ hadoop_var_prefix }}/yarn_resourcemanager.pid'
-    start_command 'sudo -u {{ hadoop_user }} {{ hadoop_distr_prefix }}/bin/yarn resourcemanager'
+    start_command '{{ hadoop_distr_prefix }}/bin/yarn resourcemanager'
 
     daemonize true
     start_timeout 10.seconds
